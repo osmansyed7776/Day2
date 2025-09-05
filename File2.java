23dd63b2883bc76ff4a3a71c2dc536b9ee3c8f55
osmansyed7776 <syedosman9866505382@gmail.com>
2025-09-05 18:35:05 +0530

    Update File2.java

--- File2.java
+++ File2.java
@@ -1,6 +1,7 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        // This line prints "Hello, World!" to the console.
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
         System.out.println("Hello, World!"); 
     }
 }
